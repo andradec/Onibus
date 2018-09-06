c10423b27e6faf267b96c7218e79281cf8304b81
Caio Andrade <caio.andrade@castmeta.local>
2018-09-06 14:30:57 -0300

    Ultimo do dia

--- Onibus/spiders/Linhas.py
+++ Onibus/spiders/Linhas.py
@@ -14,55 +14,63 @@ class LinhasSpider(scrapy.Spider):
                 link = li.xpath('./@href').extract_first()
                 yield scrapy.Request(link = link, callback=self.parse_conteudo)
 
-        next_page = response.xpath('//div[contains(@class, "block module")]/div[contains(@class, "content")]/ul/li/a/@href')
+        next_page = response.xpath('//div[contains(@class, "block module")]/div[contains(@class, "content")]/ul/li/a/@href').extract()
 
         if next_page:
-            self.log('Proxima Pagina: {}'.format(next_page.extract_first()))
-            yield scrapy.Request(
-                link = next_page.extract_first(), callback= self.parse
-            )
+            for html in next_page:
+                next_href = next_page[0]
+                next_page_url = 'http://www.ctaonline.com.br%s' % link
+                yield next_page_url
+        request = scrapy.Request(next_page_url, callback = self.parse_links)
+        request.meta['link'] = link
+                
+        
             # yield scrapy.Request(
-            #     # url = 'http://www.ctaonline.com.br%s' % link,               
+            #     link = 'http://www.ctaonline.com.br%s' % link,               
             #     callback=self.parse_category
             # )
 
+            # yield scrapy.Request(
+            #     link = next_page.extract_first(), callback = self.parse_links
+            # )
+            
     def parse_conteudo(self, response):
 
-    ##pagina toda
-    itinerarios = response.xpath('//*[@id="component"]/div/article/section[2]/div/div/div/div/div/div/div/div/div')
-    ##titulo
-    title = response.xpath('//*[@id="component"]/div/article/header/h1/a/text()').extract_first()
+        ##pagina toda
+        itinerarios = response.xpath('//*[@id="component"]/div/article/section[2]/div/div/div/div/div/div/div/div/div')    
+        ##titulo
+        title = response.xpath('//*[@id="component"]/div/article/header/h1/a/text()').extract_first()
 
-    ##Rotas
-    for iot in itinerarios:
-        rota = iot.xpath('//div[contains(@class, "wpb_wrapper")]/p')
-        for iott in rota:
-            Rotas = iott.xpath('./text()').extract_first()
-            if Rotas != None:
-                print(Rotas)
+        ##Rotas
+        for iot in itinerarios:
+            rota = iot.xpath('//div[contains(@class, "wpb_wrapper")]/p')
+            for iott in rota:
+                Rotas = iott.xpath('./text()').extract_first()
+                if Rotas != None:
+                    print(Rotas)
 
-    ##Organização dos Horários
-    for p in itinerarios:
-        var = p.xpath('.//p')
-        for pp in var:
-            organiza_horarios = pp.xpath('.//span/text()').extract()
-            print(organiza_horarios)
+        ##Organização dos Horários
+        for p in itinerarios:
+            var = p.xpath('.//p')
+            for pp in var:
+                organiza_horarios = pp.xpath('.//span/text()').extract()
+                print(organiza_horarios)
 
-    ##Recuperação de Horários
-    for p in itinerarios:
-        var = p.xpath('.//table')
-        for pp in var:
-            var1 = pp.xpath('.//tr')
-            for ppp in var1:
-                horarios = ppp.xpath('.//td/span/text()').extract()
-                print(horarios)
+        ##Recuperação de Horários
+        for p in itinerarios:
+            var = p.xpath('.//table')
+            for pp in var:
+                var1 = pp.xpath('.//tr')
+                for ppp in var1:
+                    horarios = ppp.xpath('.//td/span/text()').extract()
+                    print(horarios)
 
-    yield {
-        'title': title,
-        'Rotas': Rotas,
-        'Organização de Horários': organiza_horarios,
-        'Horários': horarios,
-    }
+        yield {
+            'title': title,
+            'Rotas': Rotas,
+            'Organização de Horários': organiza_horarios,
+            'Horários': horarios,
+        }
 
     # for iot in itinerarios:
     #     teste = iot.xpath('//div[contains(@class, "wpb_wrapper")]/p[1]/text()').extract_first()
